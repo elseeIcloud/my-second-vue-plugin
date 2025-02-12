f9a8f35f6be419bdb740ea572fab8ad4c1a10f16
DmitriyZavgorodniy <dima.zavgorodniy1@gmail.com>
2025-02-12 15:31:56 +0200

    правки по шаблону

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,47 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="content">
+      <img alt="DRUID logo" class="logo" src="./assets/logo.png" width="128" height="128" />
+      <div class="wrapper">
+        <Example msg="DRUID Plugin Example!" />
+      </div>
     </div>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
+
+<script>
+import Example from './components/Example.vue';
+
+export default {
+  components: {
+    Example,
+  },
+};
+</script>
 
 <style scoped>
 header {
-  line-height: 1.5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.content {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 1.5rem;
 }
 
 .logo {
   display: block;
-  margin: 0 auto 2rem;
+  width: 128px;
+  height: 128px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.wrapper {
+  display: flex;
+  align-items: center;
 }
 </style>
