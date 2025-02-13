00ad23df5aaaf2253a91b413764e90dfb0cae1b6
DmitriyZavgorodniy <dima.zavgorodniy1@gmail.com>
2025-02-13 09:36:52 +0200

    правки по шаблону

--- src/main.js
+++ src/main.js
@@ -4,17 +4,8 @@ import App from './App.vue';
 
 const app = createApp(App);
 
-app.config.globalProperties.$iframeData = {
-    token: null,
-    spaceId: null,
-};
-
 window.addEventListener('message', (event) => {
-    if (event.data?.token && event.data?.spaceId) {
-        app.config.globalProperties.$iframeData.token = event.data.token;
-        app.config.globalProperties.$iframeData.spaceId = event.data.spaceId;
-        console.log('Плагин получил данные:', event.data);
-    }
+    console.log('Плагин получил данные:', event.data);
 });
 
 app.mount('#app');
